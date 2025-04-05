751511520e95f8fdf9b822adfdaf4487a9cf3c33
Alfi Zamriza <alfi576@mhs.usk.ac.id>
2025-04-06 00:17:52 +0700

    feat: make item removal case-insensitive in FridgeUnit

--- FridgeUnit.java
+++ FridgeUnit.java
@@ -13,16 +13,32 @@ public class FridgeUnit implements IngredientControl, IngredientViewer {
 
     @Override
     public void removeItem(String name) {
-        if (storage.remove(name)) {
-            System.out.println(name + " dihapus dari kulkas.");
-        } else {
+        String normalizedKey = name.toLowerCase();
+        boolean found = false;
+
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).toLowerCase().equals(normalizedKey)) {
+                storage.remove(i);
+                System.out.println(name + " dihapus dari kulkas.");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
             System.out.println(name + " tidak ditemukan di kulkas.");
         }
     }
 
     @Override
     public boolean containsItem(String name) {
-        return storage.contains(name);
+        String normalizedKey = name.toLowerCase();
+        for (String item : storage) {
+            if (item.toLowerCase().equals(normalizedKey)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
